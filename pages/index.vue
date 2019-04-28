35a48d675a0f0124445eeb47b852ac442102d8fb
J. Paul Bamberg <jpbamberg1993@gmail.com>
2019-04-28 16:16:32 -0400

    Create landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,58 @@
 <template>
+  <!-- <section class="container" :style="{ backgroundImage: `url(${logo})` }"> -->
   <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        discover-mia
-      </h1>
-      <h2 class="subtitle">
-        My awesome Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
+    <div class="landing">
+      <nuxt-link :to="{ name: 'discover' }">
+        <h1 class="discover-header">
+          DISCOVER THE WORLD<br />
+          IN <span class="city">MIAMI</span>
+        </h1>
+      </nuxt-link>
     </div>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
+export default {}
 </script>
 
 <style>
+:root {
+  --black: #231f20;
+  --pink: #e984b5;
+}
+
+@font-face {
+  font-family: 'Caviar Dreams';
+  src: url('~static/fonts/CaviarDreams.ttf');
+}
+
+a {
+  color: white;
+  text-decoration: none;
+}
+
 .container {
+  font-family: 'Caviar Dreams', 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    'Helvetica Neue', Arial, sans-serif;
   margin: 0 auto;
   min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
+  background-image: url('~assets/logo.svg');
+  background-repeat: no-repeat;
+  background-position: center;
+  background-color: var(--black);
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.discover-header {
+  font-size: 30px;
+  margin-top: 70vh;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.city {
+  color: var(--pink);
 }
 </style>
